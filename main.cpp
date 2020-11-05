18e787712ab3578f1853173c28261a95d07a7889
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-11-05 12:14:20 +0100

    End to end example

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main(int argc, char *argv[]){
     std::cout << "Value in argc: " << argc << std::endl;
@@ -17,6 +19,31 @@ int main(int argc, char *argv[]){
 
     std::cout << "Input file: " << input_file << std::endl;
     std::cout << "Output file: " << output_file << std::endl;
+
+    std::ifstream source(input_file);
+
+    std::vector<std::string> lines;
+    std::string tmp;
+
+    if (source.is_open()) {
+
+        while(getline(source, tmp)) {
+            lines.push_back(tmp);
+        }
+        source.close();
+    }
+
+    // Do whatever your program has to do
+    
+    std::ofstream destination(output_file);
+
+    if (destination.is_open()) {
+        for(int i = 0; i < lines.size(); i++) {
+            destination << lines[i] << "\n";
+        }
+
+        destination.close();
+    }
 
     return 0;
 }
