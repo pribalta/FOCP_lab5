5d2bc23c0cbd630cbe818217084ab276d31691d6
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-11-05 14:15:55 +0100

    Add bfs and dfs

--- main.cpp
+++ main.cpp
@@ -40,11 +40,30 @@ void dfs(Node* node) {
     }
 }
 
+
+void bfs(Node* node) {
+    std::vector<Node*> todo{node};
+
+    while(!todo.empty()) {
+        Node* next_node = todo[0];
+        std::cout << next_node->value << std::endl;
+        next_node->visited = true;
+
+        todo.erase(todo.begin()); // Delete first element in vector
+
+        for (int i = 0; i < next_node->edges.size(); i++) {
+            if (!next_node->edges[i].next->visited) {
+                todo.push_back(next_node->edges[i].next);
+            }
+        }
+    }
+}
+
 int main(){
     std::vector<Node> graph;
 
     // Create nodes
-    Node n0(0); // ROOT
+    Node n0(0);
     Node n1(1);
     Node n2(2);
     Node n3(3);
@@ -76,7 +95,7 @@ int main(){
     n3.connect(0, &n1);
 
     // DFS - Depth First Search
-    dfs(&n0);
+    bfs(&n0);
 
     return 0;
 }
