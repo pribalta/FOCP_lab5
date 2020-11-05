d828b7e691ccfe56fa26f7f0809003587cfa4856
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-11-05 12:00:07 +0100

    Read input and output flags

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
-
 int main(int argc, char *argv[]){
     std::cout << "Value in argc: " << argc << std::endl;
 
+    std::string input_file, output_file;
+
     for (int i =0; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
+        if (std::string(argv[i]) == "-i") {
+            input_file = argv[i+1];
+        }
+
+        if (std::string(argv[i]) == "-o") {
+            output_file = argv[i+1];
+        }
     }
+
+    std::cout << "Input file: " << input_file << std::endl;
+    std::cout << "Output file: " << output_file << std::endl;
 
     return 0;
 }
