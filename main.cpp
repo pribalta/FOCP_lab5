6feb81b5a08302ce719fe52907b039647f4d35ed
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-11-05 13:21:50 +0100

    Example graph creation

--- main.cpp
+++ main.cpp
@@ -2,17 +2,51 @@
 #include <fstream>
 #include <vector>
 
-#include "main.h"
+struct Edge
+{
+    float weight;
+    Node* next;
 
-int main(int argc, char *argv[]){
-    std::string input_file = read_input_path(argc, argv);
-    std::string output_file = read_output_path(argc, argv);
+    Edge(float w, Node* n) {
+        weight = weight;
+        next = n;
+    }
+};
 
-    std::vector<std::string> lines = read_file(input_file);
+struct Node
+{
+    int value;
+    std::vector<Edge> edges;
 
-    // Do whatever your program has to do
+    Node(float v) {
+        value = v;
+    }
 
-    write_file(output_file, lines);
+    void connect(float w, Node* next) {
+        Edge e(w, next);
+        edges.push_back(e);
+    }
+};
+
+
+int main(){
+    std::vector<Node> graph;
+
+    Node n0(0);
+    Node n1(1);
+    Node n2(2);
+
+    n0.connect(1.5, &n1);
+    n0.connect(3.1, &n2);
+
+    Node new_node(123);
+
+    n0.connect(5.6, &new_node);
+
+    graph.push_back(n0);
+    graph.push_back(n1);
+    graph.push_back(n2);
+    graph.push_back(new_node);
 
     return 0;
 }
