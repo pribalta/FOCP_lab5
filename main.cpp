0b256d7ebbcb68ec0e99d0dea884df9e32ba9dee
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-11-05 11:51:14 +0100

    Inspect arguments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
-int main(){
+#include <iostream>
+
+
+int main(int argc, char *argv[]){
+    std::cout << "Value in argc: " << argc << std::endl;
+
+    for (int i =0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+
     return 0;
 }
